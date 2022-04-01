db6dc87f701f444c7cf61cc2ce93e06498b17d29
David <9019656+insertcodehere@users.noreply.github.com>
2022-04-01 15:36:47 +0100

    Fix connection issue

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
-const debug = true;
+const debug = false;
+
 const ports = {};
 
 function log(message, ...args) {
@@ -8,36 +9,48 @@ function log(message, ...args) {
 log('Background script loaded');
 log('Runtime', chrome.runtime);
 
+// Necessary
+chrome.runtime.onConnect.addListener(function (port) {
+  console.assert(port.name === 'student-attendance-channel');
+  log(ports);
+
+  if (port.sender.origin.includes('portal.ipbeja.pt')) {
+    ports['page'] = port;
+  }
+  else if (port.sender.origin.includes('chrome-extension')) {
+    ports['extension'] = port;
+  }
+
+
+  port.onMessage.addListener(function (event) {
+    if (event.source === 'POPUP') {
+      log('[Done] Relay to content-script.js', event);
+      ports['page'].postMessage(event);
+    }
+    else if (event.source === 'CONTENT_SCRIPT') {
+      log('[In progress] Relay to popup.js', event);
+      ports['extension'].postMessage(event);
+    }
+    else {
+      // Source, Destination, Id, Payload
+      log('Else', event);
+    }
+  });
+});
+
+// For debug
 chrome.runtime.onInstalled.addListener(() => {
   log('Installed');
+});
 
+chrome.runtime.onStartup.addListener(() => {
+  log('OnStartup');
+});
 
-  chrome.runtime.onConnect.addListener(function (port) {
-    console.assert(port.name === 'student-attendance-channel');
-    log(ports);
+chrome.runtime.onSuspend.addListener(() => {
+  log('OnSuspend');
+});
 
-    if (port.sender.origin.includes('portal.ipbeja.pt')) {
-      ports['page'] = port;
-    }
-    else if (port.sender.origin.includes('chrome-extension')) {
-      ports['extension'] = port;
-    }
-
-
-    port.onMessage.addListener(function (event) {
-      if (event.source === 'POPUP') {
-        log('[Done] Relay to content-script.js', event);
-        ports['page'].postMessage(event);
-      }
-      else if (event.source === 'CONTENT_SCRIPT') {
-        log('[In progress] Relay to popup.js', event);
-        ports['extension'].postMessage(event);
-      }
-      else {
-        // Source, Destination, Id, Payload
-        log('Else', event);
-      }
-    });
-  });
-
+chrome.runtime.onSuspendCanceled.addListener(() => {
+  log('OnSuspendCanceled');
 });
